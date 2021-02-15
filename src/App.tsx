64e369727488d198283f1767e281039e361e93d1
hnyk <ondrej.hnyk@t-mobile.cz>
2021-02-15 09:49:18 +0100

    long term mass

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const channels = [
   // 1154223,
 ];
 
-// <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1153527/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>
+// <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1153527/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&days=1&type=line&update=15"></iframe>
 const Ul = (channelId: number) => {
   const graphs = [
     { id: 1, color: "1fd364" },
@@ -28,14 +28,23 @@ const Ul = (channelId: number) => {
       {graphs.map((item) => {
         return (
           <iframe
-          // width="50vw"
+            // width="50vw"
             width="350"
             height="260"
-            style={{ border: "1px solid #cccccc", margin: '5px' }}
-            src={`https://thingspeak.com/channels/${channelId}/charts/${item.id}?bgcolor=%23ffffff&color=%23${item.color}&dynamic=true&width=auto&height=auto&xaxis=%C4%8Das&results=60&type=spline&update=30`}
+            style={{ border: "1px solid #cccccc", margin: "5px" }}
+            src={`https://thingspeak.com/channels/${channelId}/charts/${item.id}?bgcolor=%23ffffff&color=%23${item.color}&dynamic=true&width=auto&height=auto&xaxis=%C4%8Das&days=1&type=spline&update=30`}
           ></iframe>
         );
       })}
+      <div style={{width: "100vw"}}>
+        <iframe
+          // // width="50vw"
+          // width="1000px"
+          height="260"
+          style={{ border: "1px solid #cccccc", margin: "5px", width:"90vw" }}
+          src={`https://thingspeak.com/channels/${channelId}/charts/1?bgcolor=%23ffffff&color=%23008302&dynamic=true&width=auto&height=auto&xaxis=%C4%8Das&days=40&timescale=1440&type=spline&update=30`}
+        ></iframe>
+      </div>
     </>
   );
 };
@@ -45,7 +54,15 @@ const App = () => {
     <div className="App">
       {channels.map((channel) => {
         return (
-          <div style={{ margin:'5px', padding:'5px', border: "2px solid darkgrey" }}>{Ul(channel)}</div>
+          <div
+            style={{
+              margin: "5px",
+              padding: "5px",
+              border: "2px solid darkgrey",
+            }}
+          >
+            {Ul(channel)}
+          </div>
         );
       })}
     </div>
